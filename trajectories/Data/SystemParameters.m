f95aaeece3c7e2fd41ae33584b0230eaaf4cf4fe
Luis Badesa <luis.badesa@gmail.com>
2020-03-07 12:58:22 +0000

    Add files via upload

--- trajectories/Data/SystemParameters.m
+++ trajectories/Data/SystemParameters.m
@@ -1,18 +1,21 @@
-% Author: Luis Badesa
-
-%%
 clearvars 
 clc 
 
-% The units here are GW, kÂ£/GW and kÂ£ to keep the range of variables closer
+% The units here are GW, k£/GW and k£ to keep the range of variables closer
 
+%%
+num_CGGT = 10;
+num_OCGT = 5;
+num_Clusters = 2;
+
+num_Gen = [num_CGGT num_OCGT];
 
 %% Generation costs:
 % (In the form)
 % c = [cost_gen1 cost_gen2 cost_gen3...]';
-NLHR = [0.54*ones(1,10) 0.3*ones(1,5)]'; % kÂ£/h
-HRS = [47*ones(1,10) 200*ones(1,5)]'; % kÂ£/GWh
-stc = [1*ones(1,10) 0*ones(1,5)]'; % kÂ£
+NLHR = [0.54 0.7]'; % k£/h
+HRS = [47 500]'; % k£/GWh
+stc = [0.5 0.5]'; % k£
 
 
 %% Capacities and limits:
@@ -23,52 +26,29 @@ stc = [1*ones(1,10) 0*ones(1,5)]'; % kÂ£
 %               G2_min, G2_max;
 %               G3_min, G3_max;
 %               ...
-Gen_limits_CCGT = [0.03*ones(1,10); 0.05*ones(1,10)]';
-Gen_limits_OCGT = [0.005*ones(1,5); 0.02*ones(1,5)]';
+Gen_limits_CCGT = [0.01; 0.05]';
+Gen_limits_OCGT = [0.005; 0.01]';
 Gen_limits = [Gen_limits_CCGT;
               Gen_limits_OCGT];
           
-% Ramp limits:
-% (In the form)
-% Ramp_limits = [Ramp_down1, Ramp_up1;
-%                Ramp_down2, Ramp_up2;
-%                Ramp_down3, Ramp_up3;
-%                ...
-Ramp_CCGT = [0.03*ones(1,10); 0.03*ones(1,10)]';
-Ramp_OCGT = [0.02*ones(1,5); 0.02*ones(1,5)]';
-Ramp_limits = [Ramp_CCGT;
-              Ramp_OCGT];
+% % Ramp limits:
+% % (In the form)
+% % Ramp_limits = [Ramp_down1, Ramp_up1;
+% %                Ramp_down2, Ramp_up2;
+% %                Ramp_down3, Ramp_up3;
+% %                ...
+% Ramp_CCGT = [0.05; 0.05]';
+% Ramp_OCGT = [0.02; 0.02]';
+% Ramp_limits = [Ramp_CCGT;
+%               Ramp_OCGT];
           
 
 %% Other parameters
-VOLL = 30e3; % Units: kÂ£/GWh
+VOLL = 30e3; % Units: k£/GWh
 
 tau = 1; % 1 hour time step
 
-inflexible = [ones(1,10) zeros(1,5)]; % 1 if unit is inflexible after DA, 0 otherwise
-
-%Initial_commitment = [zeros(1,15)]';
-
-%% Error checks
-num_gen = length(HRS);
-
-% if length(Demand)~=stages
-%     error('The length of vector Demand must be equal to the number of stages (the stages come from Coursework2.m)')
-% end
-
-% if length(Initial_commitment)~=num_gen
-%     error('The lenght of vector "Initial_commitment" must be equal to the number of generators')
-% end
-
-if size(Gen_limits,1)~=num_gen
-    error('Matrices "Gen_limits" and "Ramp_limits" must have as many rows as generators defined')    
-end
-if size(Gen_limits,2)~=2
-    error('Matrices "Gen_limits" and "Ramp_limits" must only have 2 columns')    
-end
-if any(size(Gen_limits)~=size(Ramp_limits))
-    error('There is a problem with the size of matrix "Gen_limits" or "Ramp_limits"')
-end
+inflexible = [1 0]; % 1 if unit is inflexible after DA, 0 otherwise
 
 
 %%
